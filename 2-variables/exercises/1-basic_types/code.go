9451fd5d678f123bed7e1a4e837a75462cc9a734
Omsurvase24 <omsurvase24@gmail.com>
2024-09-10 20:18:18 +0530

    more variable

--- 2-variables/exercises/1-basic_types/code.go
+++ 2-variables/exercises/1-basic_types/code.go
@@ -7,7 +7,11 @@ func main() {
 	var smsSendingLimit int = 10
 	var costPerSMS float32 = 5
 	var hasPermission bool = true
-	var username string = "Om"
-
+	// var username string = "Om"
+	username := "Om"
+	name := username // name is also is a string
+	const premiumPlanName = "Premium Plan"
 	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+	fmt.Printf("%q", name)
+	fmt.Println("plan:", premiumPlanName)
 }
